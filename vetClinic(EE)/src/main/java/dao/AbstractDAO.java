5e4de407a4d686560b897b382d8a01c39bdabfbd
Korolkov Bilyal <bilyal.korolkov@gmail.com>
2020-03-16 00:03:34 +0400

    +daoPackage

--- vetClinic(EE)/src/main/java/dao/AbstractDAO.java
+++ vetClinic(EE)/src/main/java/dao/AbstractDAO.java
@@ -1,4 +1,18 @@
 package dao;
 
-public class AbstractDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class AbstractDAO implements InterfaceDAO {
+    private final String URL = "jdbc:mysql://localhost:3306/jdbcDB?useUnicode=true&serverTimezone" +
+            "=UTC&useSSL=false&allowPublicKeyRetrieval=true";
+    private final String USER = "root";
+    private final String PASSWORD = "ZXuhaha11ha";
+
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
--- vetClinic(EE)/src/main/java/dao/InterfaceDAO.java
+++ vetClinic(EE)/src/main/java/dao/InterfaceDAO.java
@@ -1,4 +1,8 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public interface InterfaceDAO {
+    Connection getConnection() throws ClassNotFoundException, SQLException;
 }
